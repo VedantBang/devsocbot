8f69a21f54f3bb1766c5d22bfefa4d12050d0ec6
VedantBang <vedantbang5@gmail.com>
2020-04-01 10:35:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Bot Invitation link https://bp-gc.in/invitebot
+
+const Discord = require('discord.js');
+const Bot = new Discord.Client();
+const config = require('./config.js');
+const methods = require('./methods.js');
+
+
+Bot.on('ready',()=>{
+	console.log(`Logged in as ${Bot.user.tag}.`);
+	Bot.user.setActivity("https://devsoc.club", {type: "WATCHING"});
+});
+
+
+Bot.on('message', msg => {
+	if(msg.content.startsWith(config.prefix) && !(msg.author.bot)){
+		let parameters = msg.content.split(" ");
+		let command = parameters.shift().substr(config.prefix.length);
+
+		if(methods.hasOwnProperty(command)) methods[command].func(msg,parameters);
+		
+	}
+});
+
+
+Bot.on('guildCreate', guild => {
+	
+});
+
+
+Bot.login(config.token);
---
+++ methods.js
@@ -0,0 +1,92 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const prefix = require('./config.js')['prefix'];
+
+// all methods of the bot
+
+function info(msg,parameters){
+	let embed = new Discord.MessageEmbed()
+	.setColor(0x00a99d)
+	.setTitle('DevSoc; Bot')
+	.setURL('https://devsoc.club/')
+	.setImage('https://i.ibb.co/XkMC3bR/logo.png')
+	.addField('Created By', 'Developers\' Society BITS Goa');
+	msg.channel.send(embed);
+}
+
+
+function help(msg,parameters){
+	let embed = new Discord.MessageEmbed()
+	.setColor(0x00a99d)
+	.setTitle('Help Menu')
+	.addField('Bot Prefix', prefix);
+	for(let key in methods){
+		embed.addField(key, methods[key].desc);
+	}
+	msg.channel.send(embed);
+}
+
+
+async function ping(msg,parametes){
+	try{
+		let m = await msg.channel.send("Pong!");
+		let embed = new Discord.MessageEmbed()
+		.setColor(0x00a99d)
+		.addField("Bot Latency", `${m.createdTimestamp - msg.createdTimestamp}ms`, true)
+		.addField("API Latency", `${m.client.ws.ping}ms`, true);
+		msg.channel.send(embed);
+	}
+	catch(err){
+		msg.channel.send("An error occured. Please try again.");
+		console.log(err);
+	}
+}
+
+
+async function shorten(msg,parameters){
+	try{
+		let body = {
+			longUrl: parameters[0]
+		}
+		if(parameters[1]) body.customCode = parameters[1];
+		let response = await fetch('https://bp-gc.in/api/url/shorten',{
+			method: 'POST',
+			headers: {
+				'Content-Type':'application/json;charset=UTF-8'
+			},
+			body: JSON.stringify(body)
+		});
+		response = await response.json();
+		let embed = new Discord.MessageEmbed()
+		.setColor(0x00a99d)
+		.setTitle('URL Shortener')
+		.addField('Short URL', response.shortUrl);
+		msg.channel.send(embed);
+	}
+	catch(err){
+		msg.channel.send("An error occured. Please try again.");
+		console.log(err);
+	}
+}
+
+
+const methods = {
+	info: {
+		func: info,
+		desc: 'Displays general information about the Bot'
+	},
+	help: {
+		func: help,
+		desc: 'Displays the Help Menu'
+	},
+	shorten: {
+		func: shorten,
+		desc: `Shorten any URL\nUsage: ${prefix}shorten <URL>\nCan also create custom shortened links: ${prefix}shorten <URL> <custom name>`
+	},
+	ping: {
+		func: ping,
+		desc: 'Displays the Bot Latency and Discord API Latency'
+	}
+}
+
+module.exports = methods;
