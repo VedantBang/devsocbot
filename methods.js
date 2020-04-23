b250d4acfe07b13279369ab5ba81841445c4a7f8
VedantBang <vedantbang5@gmail.com>
2020-04-24 01:16:24 +0530

    refine mines

--- methods.js
+++ methods.js
@@ -85,10 +85,11 @@ async function minesweeper(msg,parameters){
 	try{
 		let n = parseInt(parameters[0]);
 		if(Number.isInteger(n) && n > 3 && n < 12){
-			msg.channel.send("Please wait for 10 seconds").then(async m=>{
+			msg.channel.send("Generating random field, wait 10 seconds...").then(async m=>{
 				mines(msg,n);
 				await delay(8000);
 				m.edit("Ready!");
+				msg.channel.send("\`Note: Borders contain no bombs.\`");
 			});
 		} else {
 			msg.channel.send("\`Input should be an integer between 3 and 12.\`");
